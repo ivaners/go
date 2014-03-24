8ef1ee68d4d905431faca7102cb101388ed1cf3d
GitHub for Windows - 3F-IT-2056 <a4:6f:ce:fb:82:67:7b:33:6d:54:a9:c1:2f:ce:1c:99>
2014-03-24 16:39:07 +0800

    增加sqlite example

--- example.go
+++ example.go
@@ -20,13 +20,15 @@ import (
 	"os"
 	// "reflect"
 	"regexp"
-	"strings"
+	// "strings"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func read(path string) string {
 	fi, err := os.Open(path)
 	if err != nil {
-		panic(err)
+		fmt.Println("文件打开出错，可能找不到文件", path)
 	}
 	defer fi.Close()
 	r := bufio.NewReader(fi)
@@ -79,8 +81,76 @@ func getRequest() {
 	}
 }
 
-func main() {
-	ipList := strings.Split(read("t.txt"), "\n")
-	fmt.Println(ipList[0])
+func sqlite() {
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkErr(err)
 
+	//插入数据
+	stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
+	checkErr(err)
+
+	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+	fmt.Println(id)
+	//更新数据
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("astaxieupdate", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	//查询数据
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
+	}
+
+	//删除数据
+	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	// ipList := strings.Split(read("t.txt"), "\n")
+	// fmt.Println(ipList[0])
+	// txt := read("t.txt")
+
+	// if txt == "" {
+	// 	return
+	// }
+	// fmt.Println("...")
+	sqlite()
 }
